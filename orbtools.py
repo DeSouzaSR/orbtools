700ad8520cb1548b95c0f8e65f8a8bf290e0c8f5
DeSouzaSR <sandro.fisica@gmail.com>
2018-08-27 15:39:48 -0300

    Create a2p and p2a functions

---
+++ orbtools.py
@@ -0,0 +1,27 @@
+"""
+Converts semi-major axis in period.
+a: semimajor axis in au (astronomical unit)
+p: period in a (year == annus)
+""" 
+from math import sqrt
+
+def a2p(a):
+    return sqrt(a**3)
+
+
+def p2a(p):
+    return (p**2)**(1/3)
+
+
+def main():
+    print("convert  a = 1.00000011")
+    a = 1.00000011 # au
+    print(a2p(a))
+
+    print("convert  p = 1.0000001650000045")
+    a = 1.0000001650000045 # au
+    print(p2a(a))
+
+
+if __name__ == '__main__':
+    main()
